f8d09ff7b1f5ec54a197737e0f66ddfd425876b6
Vinay Kumar.M <vinay.munivenkata@gmail.com>
2025-09-25 16:14:11 +0530

    Update storage.tf

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,30 +1,18 @@
-# Random ID to ensure global uniqueness
-resource "random_id" "id" {
-  byte_length = 4
-}
-
-# S3 Bucket
 resource "aws_s3_bucket" "db_backups" {
   bucket = "interview-db-backups-${random_id.id.hex}"
+  force_destroy = true
 }
 
-# Disable public access blocks so the bucket can be public
+# Disable public access blocking
 resource "aws_s3_bucket_public_access_block" "disable_public_block" {
   bucket = aws_s3_bucket.db_backups.id
-
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
 
-# Make bucket objects publicly readable
-resource "aws_s3_bucket_acl" "db_backups_acl" {
-  bucket = aws_s3_bucket.db_backups.id
-  acl    = "public-read"
-}
-
-# Public bucket policy
+# Make bucket public
 resource "aws_s3_bucket_policy" "public_policy" {
   bucket = aws_s3_bucket.db_backups.id
   policy = jsonencode({
