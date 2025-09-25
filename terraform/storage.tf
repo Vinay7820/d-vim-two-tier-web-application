5c89f08c059c950efea622c97acbde62abec2e07
Vinay Kumar.M <vinay.munivenkata@gmail.com>
2025-09-25 15:28:52 +0530

    Update storage.tf

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -8,14 +8,30 @@ resource "aws_s3_bucket_acl" "db_backups_acl" {
   acl    = "public-read"
 }
 
-resource "aws_s3_bucket_public_access_block" "block_public_access" {
-  bucket = aws_s3_bucket.db_backups.id
+#resource "aws_s3_bucket_public_access_block" "block_public_access" {
+#  bucket = aws_s3_bucket.db_backups.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  #block_public_acls       = false
+  #block_public_policy     = false
+  #ignore_public_acls      = false
+  #restrict_public_buckets = false
+#}
+
+resource "aws_s3_bucket_policy" "db_backups_policy" {
+  bucket = aws_s3_bucket.db_backups.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = ["s3:GetObject"]
+        Resource  = "${aws_s3_bucket.db_backups.arn}/*"
+      }
+    ]
+  })
 }
+
 
 resource "aws_s3_bucket_policy" "public_policy" {
   bucket = aws_s3_bucket.db_backups.id
